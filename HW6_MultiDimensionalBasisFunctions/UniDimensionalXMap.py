1635b46b88913f09c5b1b239f0bd8d1d43e9c18f
Caleb Jones <calebajones103@gmail.com>
2025-10-15 08:16:44 -0600

    Add files via upload

--- HW6_MultiDimensionalBasisFunctions/UniDimensionalXMap.py
+++ HW6_MultiDimensionalBasisFunctions/UniDimensionalXMap.py
@@ -14,12 +14,17 @@ from matplotlib import pyplot as plt
 
 # IMPORT (or copy) your code from HW3 here
 # which evaluated a Lagrane polynomial basis function
-# import <UNCOMMENT THIS LINE AND INPUT YOUR FILENAME HERE>
+import MultiDimensionalBasisFunctions
+import Univariate_Lagrange_Basis_Functions
 
 # x(\xi) = \sum_{a=0}^p x_a * N_a(\xi)
 def XMap(deg,spatial_pts,interp_pts,xi):
-    # complete this function
-    return
+    x_e = 0
+    for a in range(deg+1):
+# what to plug in for a?? are my plots what I am supposed to see??? what does the question at the end mean?
+        x_e += spatial_pts[a]*Univariate_Lagrange_Basis_Functions.LagrangeBasisEvaluation(deg, interp_pts, xi, a)
+
+    return x_e
 
 def PlotXMap(deg,spatial_pts,interp_pts, npts=101, contours=True):
     # parametric points to evaluate in Lagrange basis function
@@ -33,7 +38,7 @@ def PlotXMap(deg,spatial_pts,interp_pts, npts=101, contours=True):
             xs.append(XMap(deg,spatial_pts,interp_pts,xi))
         
         plt.plot(xi_vals,xs)
-        plt.show()
+        plt.savefig("hw6_task2_1")
 
     # evaluate as a contour plot
     else:
@@ -50,4 +55,18 @@ def PlotXMap(deg,spatial_pts,interp_pts, npts=101, contours=True):
         ax.set_xlabel(r"$x$")
         ax.yaxis.set_ticklabels([])
         fig.colorbar(surf)
-        plt.show()
+        plt.savefig("hw6_task2_1")
+
+
+
+xa = [0, 1]
+ca = [-1, 1]
+#PlotXMap(1, xa, ca, contours=False)
+xa2 = [.5, 1, 1.5]
+ca2 = [-1, 0, 1]
+#PlotXMap(2, xa2, ca2, contours=False)
+xa3 = [.5, .7, 1.5]
+ca3 = [-1, -.6, 1]
+#PlotXMap(2, xa3, ca3, contours=False)
+ca4 = [-1, 0, 1]
+#PlotXMap(2, xa3, ca4, contours=False)
